da4450633be037e1e6899099641414638e099451
yichenliuuuu <37308658+yichenliuuuu@users.noreply.github.com>
2018-03-14 11:19:29 -0700

    Update Magpie4.java

--- MagpieActivityStarterCode/activity4/Magpie4.java
+++ MagpieActivityStarterCode/activity4/Magpie4.java
@@ -64,7 +64,12 @@ public class Magpie4
 					&& findKeyword(statement, "me", psn) >= 0)
 			{
 				response = transformYouMeStatement(statement);
-			}
+		        else if (findKeyword(statement, "I", 0) >=0 
+                                        && findKeyword(statement, "you", findKeyword(statement, "I", 0)) >=0)
+                        { 
+                            response = transformIYouStatement(statement);
+                        }
+			
 			else
 			{
 				response = getRandomResponse();
@@ -122,7 +127,23 @@ public class Magpie4
 		return "What makes you think that I " + restOfStatement + " you?";
 	}
 	
-	
+	private String transformIYouStatement(String statement) 
+        { 
+            statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		
+		int psnOfI = findKeyword (statement, "I", 0);
+		int psnOfYou = findKeyword (statement, "you", psnOfI + 1);
+		
+		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+		return "Why do you " + restOfStatement + " me?";
+        }      
 
 	
 	
